15a4395ae09240d52e0486bad02e3be9e282bb14
Florian <florian@ubuntu>
2017-06-09 09:44:49 +0200

    Changed IP Class for 64bit. Book is about 32bit

--- sniffer_ip_header_decode.py
+++ sniffer_ip_header_decode.py
@@ -5,55 +5,58 @@ import struct
 from ctypes import *
 
 #host to listen on
-host = "192.168.2.163"
+host = "131.159.219.58"
 
 #our ip header
+#for 64bit OS only!
 class IP(Structure):
-	_fields_ = [
-		("ihl",			c_ubyte,4),
-		("version",		c_ubyte,4),
-		("tos",			c_ubyte),
-		("len",			c_ushort),
-		("id",			c_ushort),
-		("offset",		c_ushort),
-		("ttl",			c_ubyte),
-		("protocol_num",c_ubyte),
-		("sum",			c_ushort),
-		("src",			c_ulong),
-		("dst",			c_ulong)
-	]
 
-def __new__(self, socket_buffer=None):
-	return self.from_buffer_copy(socket_buffer)
+    _fields_ = [
+        ("ihl",           c_uint8, 4),
+        ("version",       c_uint8, 4),
+        ("tos",           c_uint8),
+        ("len",           c_uint16),
+        ("id",            c_uint16),
+        ("offset",        c_uint16),
+        ("ttl",           c_uint8),
+        ("protocol_num",  c_uint8),
+        ("sum",           c_uint16),
+        ("src",           c_uint32),
+        ("dst",           c_uint32)
+    ]
 
-def __init__(self,socket_buffer=None):
-	print "In __init__"
-	#map protocol constants to their names
-	self.protocol_map = {1:"ICMP", 6:"TCP",17:"UDP"}
+    def __new__(self, socket_buffer=None):
+        return self.from_buffer_copy(socket_buffer)    
 
-	#human readable IP addresses
-	self.src_address = socket.inet_ntoa(struct.pack("<L", self.src))
-	self.dst_address = socket.inet_ntoa(struct.pack("<L", self.dst))
+    def __init__(self, socket_buffer=None):
 
-	#human readable protocol
-	try:
-		self.protocol = self.protocol_map[self.protocol_num]
-	except:
-		print "Exception thrown"
-		self.protocol = str(self.protocol_num)
+        # map protocol constants to their names
+        self.protocol_map = {1:"ICMP", 6:"TCP", 17:"UDP"}
+
+        # human readable IP addresses
+        self.src_address = socket.inet_ntoa(struct.pack("<L",self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("<L",self.dst))
+
+        # human readable protocol
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
 
 if os.name == "nt":
-	socket_protocol = socket.IPPROTO_IP
+    socket_protocol = socket.IPPROTO_IP 
 else:
-	socket_protocol = socket.IPPROTO_ICMP
-
+    socket_protocol = socket.IPPROTO_ICMP
+    
 sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
 
-sniffer.bind((host,0))
+sniffer.bind((host, 0))
+
+# we want the IP headers included in the capture
 sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
-#if we're using Windows ,we neet to send an IOCTL
-#to set up promiscuous mode
+# if we're on Windows we need to send some ioctls
+# to setup promiscuous mode
 if os.name == "nt":
 	sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
@@ -62,13 +65,10 @@ try:
 	while True:
 			#read a package
 			raw_buffer = sniffer.recvfrom(65565)[0]
-
-
-			#create an IP header from the first 20 bytes of the buffer
-			tmp = raw_buffer[0:20]
-			print type(tmp)
-			print raw_buffer
-			ip_header = IP(tmp)
+    
+        # create an IP header from the first 20 bytes of the buffer
+			ip_header = IP(raw_buffer[0:20])
+			
 			#print out the protocol that was detected and the hosts 
 			print "Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
 
